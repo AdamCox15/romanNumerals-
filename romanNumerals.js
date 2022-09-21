6c555922a3a52bce0f59a05aed71d6f6e1312d25
Adam Cox <adamcox.dev@gmail.com>
2022-09-21 10:10:08 -0600

    Second commit first solution not 100% right

--- romanNumerals.js
+++ romanNumerals.js
@@ -48,3 +48,29 @@
 // toRoman(99)
 // 'LXXXXVIIII'
 
+const integer_to_roman =(num) =>{
+	const obj = {0:['M',1000], 1:['D', 500], 2:['C', 100], 3:['L', 50], 4:['X', 10], 5:['V', 5], 6:['I', 1]};
+	let arr = [];
+	for (let i in obj){
+		let count = 0;
+		if (num >= obj[i][1]){
+			count = Math.floor(num / obj[i][1]);
+			num -= count*obj[i][1];
+			if (count===4 && (obj[i][0]==='C'||obj[i][0]==='X'||obj[i][0]==='I')){
+				if(arr[arr.length-1]===obj[i-1][0]){
+					arr.pop();
+					arr.push(obj[i][0], obj[i-2][0]);
+					continue;	
+				}
+				arr.push(obj[i][0], obj[i-1][0]);
+				continue;
+			}
+			for (let j=1; j<=count; j++){
+				arr.push(obj[i][0])
+			}
+		} 
+	}
+	return arr.join('');
+}
+
+console.log(integer_to_roman(4));
